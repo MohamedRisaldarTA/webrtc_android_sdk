692ce46c8d23ee72f9dd935841c14793d3ec62ea
MohamedRisaldarTA <73091075+MohamedRisaldarTA@users.noreply.github.com>
2022-07-05 16:34:06 +0530

    Create custom_setup.sh

---
+++ custom_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+VERSION=1.0
+
+RELEASE_VERSION=${VERSION}
+
+SDK_BIN_URL=https://github.com/MohamedRisaldarTA/webrtc_android_sdk/releases/download/${VERSION}/libwebrtc.aar
+
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+echo "Downloading webrtc-sdk ${VERSION} binary for android."
+curl -L -O ${SDK_BIN_URL}
+
+mvn install:install-file \
+    -Dfile=libwebrtc.aar \
+    -Dpackaging=aar \
+    -Dversion=${RELEASE_VERSION} \
+    -DgroupId=com.github.MohamedRisaldarTA \
+    -DartifactId=webrtc_android_sdk
